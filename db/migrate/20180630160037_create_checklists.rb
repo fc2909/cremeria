e0c4711be06156013c1b31d51631b8322af3394d
rails v <rails -v>
2018-06-30 11:02:13 -0500

    Commit message

---
+++ db/migrate/20180630160037_create_checklists.rb
@@ -0,0 +1,70 @@
+class CreateChecklists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :checklists do |t|
+      t.string :nombre
+      t.string :fecha
+      t.integer :n1
+      t.integer :n2
+      t.integer :n3
+      t.integer :n4
+      t.integer :n5
+      t.integer :n6
+      t.integer :n7
+      t.integer :n8
+      t.integer :n9
+      t.integer :n10
+      t.integer :n11
+      t.integer :n12
+      t.integer :n13
+      t.integer :n14
+      t.integer :n15
+      t.integer :n16
+      t.integer :n17
+      t.integer :n18
+      t.integer :n19
+      t.integer :n20
+      t.integer :n21
+      t.integer :n22
+      t.integer :n23
+      t.integer :n24
+      t.integer :n25
+      t.integer :n26
+      t.integer :n27
+      t.integer :n28
+      t.integer :n29
+      t.integer :n30
+      t.integer :n31
+      t.integer :n32
+      t.integer :n33
+      t.integer :n34
+      t.integer :n35
+      t.integer :n36
+      t.integer :n37
+      t.integer :n38
+      t.integer :n39
+      t.integer :n40
+      t.integer :n41
+      t.integer :n42
+      t.integer :n43
+      t.integer :n44
+      t.integer :n45
+      t.integer :n46
+      t.integer :n47
+      t.integer :n48
+      t.integer :n49
+      t.integer :n50
+      t.integer :n51
+      t.integer :n52
+      t.integer :n53
+      t.integer :n54
+      t.integer :n55
+      t.integer :n56
+      t.integer :n57
+      t.integer :n58
+      t.integer :n59
+      t.integer :n60
+
+      t.timestamps
+    end
+  end
+end

05b1327890892143692ada5bd5354cac1d10542b
rails v <rails -v>
2018-08-04 12:40:28 -0500

    Commit message

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -30538,7 +30538,7 @@ function selectMermaP3(id, valorM,h){
   idVentas2=id;
   var peso = ' <label class=" impre letras " >PESO</label><input type="text" id="pesoMP" class=" impre form-control clear pesoMP" onchange="document.getElementById("addMP").focus();" placeholder="">';
  var delventas1 = '<button type="button" class="btn btn-ventas btn-ventas eliminar" onclick="delMermaP2()">ELIMINAR</button>';
-  alert("30541");
+ 
   var seleccionado;
   for(var a=0; a<arrGlobalMerma.length; a++){
     if(arrGlobalMerma[a].id == id){
@@ -30566,6 +30566,7 @@ function selectDeg2(id, valorM,h){
   var peso = ' <label class=" impre letras " >PESO</label><input type="text" id="pesoMP" class=" impre form-control clear pesoMP" onchange="document.getElementById("addMP").focus();" placeholder="">';
  var delventas1 = '<button type="button" class="btn btn-ventas btn-ventas eliminar" onclick="delDeg2()">ELIMINAR</button>';
   var seleccionado;
+   alert("30541");
   for(var a=0; a<arrGlobalMerma.length; a++){
     if(arrGlobalMerma[a].id == id){
        $(".upventaMP").html(delventas1);

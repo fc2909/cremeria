6fa214592fcf2931aed119f2768ff608976a7f4d
rails v <rails -v>
2018-08-06 11:05:33 -0500

    Commit message

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -18366,7 +18366,7 @@ identificacionP= '<tr style="font-size:10px; " class="text-center" ><td><strong>
 
         }
 ventasT+=t_venta_mercaT;
-alert(ventasT);
+alert(s_vent);
 creditosT+=parseFloat(credits);
 bonificacionTotal+=parseFloat(bonificacionT);
 bonificacionT=0;
@@ -18640,8 +18640,6 @@ if(arrGlobalCategoria[y].descripcion==2){
 if(html!=""){
   credi+= parseFloat(creditosT);
           boni+=parseFloat(bonificacionTotal);
-alert(ventasT);
-          
           s_vent+=parseFloat(ventasT);
   html+='<tr  style="background:black; font-size:12px;"><td>TOTAL</td> <td>RESTAURANTE</td><td></td><td>$ '+formatoMoneda1(creditosT)+'</td> <td>$ '+formatoMoneda1(bonificacionTotal)+'</td><td></td><td>$ '+formatoMoneda1(ventasT)+'</td> '+productoTotalS+'</tr>';
 htmlP+='<tr class="grisclaro" style="font-size:10px;   " class=" text-center" ><td><strong>TOTAL</strong></td> <td class=" text-center" ><strong>RESTAURANTES</strong></td><td></td><td class=" text-right" ><strong>$ '+formatoMoneda1(creditosT)+'</strong></td> <td class=" text-right" ><strong>$ '+formatoMoneda1(bonificacionTotal)+'</strong></td><td></td><td class=" text-right" ><strong>$ '+formatoMoneda1(ventasT)+'</strong></td> '+productoTotalSP+'</tr>';

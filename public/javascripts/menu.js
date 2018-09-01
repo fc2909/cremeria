36305594e2d064d5e7d747bacc398dfbf1202e41
Joel Lucero <Joel.Lucero@marcasti.com>
2018-09-01 14:01:17 -0500

    Commit message

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -26052,7 +26052,7 @@ totalContado = total;
               //arrGlobal2 = lista;
               $('.imprimir').html('');
 //        document.getElementById('loader').style.display = 'none';
-var json={where:{fechaf:fechaf}};
+var json={where:{fechaf:today_v}};
       executeFunctionDone(json, 'ventaspasada', "Ocurrio un error al cargar el formulario, reintentar más tarde. ", loadRemision);
 
 }
